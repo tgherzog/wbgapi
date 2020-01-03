e367ddf176f61bcd68d636bfce38eb3368df4faa
Tim Herzog <timgherzog@gmail.com>
2020-01-03 00:08:12 -0500

    Move files to subdirectory to prepare for package assembly

---
+++ setup.py
@@ -0,0 +1,25 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wbgapi",
+    version="0.1.0a1",
+    author="Tim Herzog",
+    # author_email="author@example.com",
+    description="wbgapi provides a comprehensive interface to the World Bank's daeta and metadata APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tgherzog/wbgapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+    ],
+    install_requires=['requests'],
+    python_requires='>=3.0',
+)
