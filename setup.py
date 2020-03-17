1df774fec93138bbb2f899c97e3d52c22d8fdd18
Tim Herzog <timgherzog@gmail.com>
2020-03-16 21:12:14 -0400

    Add include_package_data in setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/tgherzog/wbgapi",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
